b273c66c37a7dfb5a173581b5aa2401be2f1a709
Sinhbapbap <32121511+Sinhbapbap@users.noreply.github.com>
2017-10-22 17:16:42 +0700

    Add files via upload

--- nhapxuatmang.c
+++ nhapxuatmang.c
@@ -5,39 +5,8 @@
 
 
 
-void main()
-{
- int a[MAX];
- int n, x;
- printf("nhap vao phan tu can tim kiem: ");
- scanf("%d", &x);
-
- bool  check= TimKiem(a,n,x);
- /*kiem tra so sanh ket qua */
- if (Check= true)
-      printf(" co phan tu %d trong mang", x);
- else
-      printf(" khong co phan tu %d trong mang", x);
- do
- {
-
-       // nhap so luong phan tu trong mang
-       printf("nhap so luong phan tu cua mang: ");
-       scanf("%d", &n);
-       if( n<0 || n>100)
-       {
-            printf("ban nhap sai so luong roi!, xin kiem tra lai");
-       }
-
- } while( n<0 || n>100);//
- Nhapmang(a, n);
- Xuatmang(a, n);
 
 
-getch();
-
-
-}
 
  void Nhapmang( int a[], int n)
 
@@ -65,13 +34,51 @@ void Xuatmang( int a[], int n)
 
 }
 
-
-bool TimKiem(int a[],int n, int x)
-{for(i=0 ; i<n ; i++)
+int TimKiem(int a[],int n, int x)
+{int i;
+for(i=0 ; i<n ; i++)
 {
       if(a[i]==x)
-            return true;
+            return 1;
 }
-return false;
+return 0;
 }
+
+
+void main()
+{
+ int a[MAX];
+ int n, x, i;
+ printf("nhap vao phan tu can tim kiem: ");
+ scanf("%d", &x);
+
+ int  check=TimKiem(a,n,x);
+ /*kiem tra so sanh ket qua */
+ if (check=1)
+      printf(" co phan tu %d trong mang", x);
+ else
+      printf(" khong co phan tu %d trong mang", x);
+ do
+ {
+
+       // nhap so luong phan tu trong mang
+       printf("nhap so luong phan tu cua mang: ");
+       scanf("%d", &n);
+       if( n<0 || n>100)
+       {
+            printf("ban nhap sai so luong roi!, xin kiem tra lai");
+       }
+
+ } while( n<0 || n>100);//
+ Nhapmang(a, n);
+ Xuatmang(a, n);
+
+
+getch();
+
+
+}
+
+
+
 
